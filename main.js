21f3f9c982d4150bb4e1b8dc8123b2d67edeee16
Adam Back <adb33@email.arizona.edu>
2014-10-12 22:10:57 -0700

    Added controlled, non-functioning. Submit button is disabled if certain conditions haven't been met.

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 var app = angular.module('myApp', []);
+
+myApp.controller('mainController', function ($scope) {
+  $scope.submitForm = function(isValid) {
+    if(isValid) {
+      alert('You entered all of the correct information! Congrats!')
+    }
+  };
+})
